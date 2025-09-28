4453493fbb201ce218afa342d7fcd56217021e53
Himanshu <himanshu16397@gmail.com>
2025-09-28 23:54:12 +0530

    update sorting

--- sorting.py
+++ sorting.py
@@ -12,34 +12,132 @@ a = [2,3,1,4,6]
 selectionsort(a)
 print(a)
 #%%
-def merge(a, i, mid, j):
-    c = []
-    low = i 
-    high = mid + 1
-    while low <= mid and high <= j:
-        if a[low]  <= a[high]:
-            c.append(a[low])
-            low += 1
+def merge(a, low, mid, high):
+    i = low
+    j = mid  + 1
+    temp = []
+    while i <= mid and j <= high:
+        if a[i] <= a[j]:
+            temp.append(a[i])
+            i += 1
         else:
-            c.append(a[high])
-            high += 1
-        
+            temp.append(a[j])
+            j += 1
+    while i <= mid:
+        temp.append(a[i])
+        i += 1
+    while j <= mid:
+        temp.append(a[j])
+        j += 1
 
-    while low <= mid:
-        c.append(a[low])
-    while high <= j:
-        c.append(a[high])
+    for k in range(len(temp)):
+        a[low + k] = temp[k]
 
-    for k in range(len(c)):
-        a[low + k] = c[k] 
+def merge_sort(a, low, high):
+    if low == high:
+        return
+    mid = (low + high) // 2
+    merge_sort(a, low, mid)
+    merge_sort(a, mid + 1, high)
+    merge(a, low, mid, high)
 
-def mergesort(a, i, j):
-    if i == j:return
-    mid = (i + j) // 2
-    mergesort(a, i, mid)
-    mergesort(a, mid + 1, j)
-    merge(a,i, mid, j)
-
-a = [2,7,5]
-mergesort(a,0,3)
+a = [2,7,5,0,9,-1,-3,100]
+merge_sort(a,0,len(a) - 1)
 print(a)
+#%%
+def bubble_sort(a):
+    for i in range(len(a)):
+        for j in range(0, len(a) - i - 1):
+            if a[j] > a[j + 1]:
+                a[j + 1], a[j] = a[j], a[j + 1]
+
+a = [100, 1, 3, 2, 10 , -1, -1]
+bubble_sort(a)
+print(a)
+
+#%%
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while( j >= 0 and a[j] > key):
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+
+a= [100, 1, 3, 2, 10 , -1, -1]
+insertion_sort(a)
+print(a)      
+def rec_insert(a, size = None):
+    if n == None:
+        n = len(a)
+    if len(a) <= 1:
+        return 
+    rec_insert(a, n - 1)
+    # else place last element in correct place
+    last = a[n - 1]
+    j = n - 2
+    while j >= 0 and a[j] > last:
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = last
+a= [100, 1, 3, 2, 10 , -1, -1]
+insertion_sort(a)
+print(a) 
+
+#%%
+a = [3,1,10,1,6,7,8]
+def partition(a, low = None, high = None):
+    if low == None or high == None: low, high = 0, len(a) - 1
+
+    i = low + 1
+    pivot = a[low]
+    j = high
+    while(i <= j):
+        while(i <= j and a[i] <= pivot): i += 1
+        while(i <= j and a[j] > pivot): j -= 1
+        if(i<=j):
+            a[i], a[j] = a[j], a[i]
+            i += 1
+            j -= 1
+    
+    a[low], a[j] = a[j], a[low]
+    return j
+
+print(partition(a))
+print(a)
+
+def quicksort(a, low = None, high = None):
+    if low == None or high == None:
+        low = 0
+        high = len(a) - 1
+    if low >= high: return
+    p = partition(a, low, high)
+    quicksort(a, low, p - 1)
+    quicksort(a, p + 1, high)
+
+b = [5,4,3,2,1, -1,-1,-2]
+quicksort(b)
+print(b)
+# %%
+# binary search
+def binary_search(a, key, low = None, high = None):
+    if low == None or high == None:
+        low = 0 
+        high = len(a) - 1  
+
+    if low  > high:
+        return -1
+    mid = (low + high) // 2
+
+    if key == a[mid]:
+        return mid
+    
+    elif key < a[mid]: return binary_search(a, key, low, mid)
+    elif key > a[mid]: return binary_search(a, key, mid + 1, high)
+
+a = [-1,-1,1,2,3,4,5]
+
+binary_search(a, 4)
+
+#%%
